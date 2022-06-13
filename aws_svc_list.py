8951fd01893fc44a29532d489d9fc160b2608323
moxiecloud <102683952+moxiecloud@users.noreply.github.com>
2022-06-13 17:34:18 -0400

    Add files via upload
    This python program creates a list of AWS services, applies functions append, insert and remove to add, insert and remove list items. Additionally, includes best practice of creating functions that perform the core processing, allowing for reuse and clean calls from __name__ == '__main__'.

---
+++ aws_svc_list.py
@@ -0,0 +1,55 @@
+# create an inventory of AWS services
+# create an empty list
+# populate list using append or insert
+# print list and length of the list
+# Remove two specific services from the list by
+#  name or by index
+# Print the new list and the new length of the list
+
+# create the initial list of aws services
+def initial_list():
+    # initialize list empty list
+    aws_services = []
+
+    # populate list with a subset of AWS services
+    aws_services.append('EC2')
+    aws_services.append('Lambda')
+    aws_services.append('S3')
+    aws_services.append('RDS')
+    aws_services.append('Fargate')
+    aws_services.append('DynamoDB')
+    aws_services.append('RedShift')
+    return(aws_services)
+
+# insert a new service
+def insert_newservice(exist_list):
+    exist_list.insert((len(exist_list)+ 1), 'SageMaker')
+    return(exist_list)
+
+# remove a service from list
+def remove_service(exist_list):
+    exist_list.remove('EC2')
+    return(exist_list)
+
+def print_service_list(service_list, list_action):
+    if list_action == "initial":
+      print('\nThe AWS service initial list includes: \n' + str(aws_svc_list))
+      print('My initial list of AWS Services includes ' + str(len(aws_svc_list)) + ' services.\n')
+    elif list_action == "append":
+      print("The new AWS service list includes: " + str(aws_svc_list) + " and includes " + str(len(aws_svc_list)) + " services.\n")
+    elif list_action == 'remove':
+      print("The AWS service list with one item removed includes: " + str(aws_svc_list) + " list includes " + str(len(aws_svc_list)) + " services.\n")
+    return()
+
+if __name__ == '__main__':
+    # initial service list and print list of services + plus length of the list
+    aws_svc_list = initial_list()
+    print_service_list(aws_svc_list, "initial")
+
+    # insert new service and print new list + plus the length list
+    aws_svc_list = insert_newservice(aws_svc_list)
+    print_service_list(aws_svc_list, "append")
+
+    # remove a service from list and then print list with item removed + length of list
+    aws_svc_list = remove_service(aws_svc_list)
+    print_service_list(aws_svc_list, "remove")
